cc10cb48e193a18db7d712870d34bca92bb2cc74
LOBUTO <jzamalloa1@gmail.com>
2015-01-08 18:10:57 -0500

    processing of chen replication time file to hugo symbols

---
+++ CHEN.REPLICATION.GENE.ASSIGN.sh
@@ -0,0 +1,6 @@
+#CHEN.REPLICATION.GENE.ASSIGN.sh
+#010815
+#Assignment of hugo symbols to replication times from the Chen et al. paper ("Impact of replication timing on non-CpG and CpG substitution rates in mammalian genomes")
+#$@ = chen.file, outfile
+#chen.file as in S50.100kbp.Non-overlappingWindows.hg18.wig
+
