13f6e93bd6eafb9e7bf3012ccbd5ea1d5a04a94d
LOBUTO <jzamalloa1@gmail.com>
2015-01-19 16:21:25 -0500

    fixed for synonyms, gain from 10845 to 10912 in tcga, not much but it's somethinggit add Function.BIOGRID.PROCESSING.R

--- Function.BIOGRID.PROCESSING.R
+++ Function.BIOGRID.PROCESSING.R
@@ -15,20 +15,36 @@ setnames(biogrid, c("A","B", "SYNA","SYNB"))
 setkey(biogrid)
 biogrid<-unique(biogrid)
 
-#Work synonyms
+#Fix for absent synonyms
+biogrid$SYNA<-ifelse(biogrid$SYNA=="-", biogrid$A, biogrid$SYNA)
+biogrid$SYNB<-ifelse(biogrid$SYNB=="-", biogrid$B, biogrid$SYNB)
+
+#Extract synonyms
 biogrid<-biogrid[,list(Hugo.A=unique(c(A, unlist(strsplit(SYNA,"|",fixed=T))))),by=c("A","B","SYNA", "SYNB")]
 biogrid<-biogrid[,list(Hugo.B=unique(c(B, unlist(strsplit(SYNB,"|",fixed=T))))),by=c("A","B","SYNA", "SYNB","Hugo.A")]
 
+#Clean up
+setkey(biogrid)
+biogrid<-unique(biogrid)
+
 #Work on complementary to eliminate doubles
-biogrid.comp<-biogrid[,c("B","A"),with=F]
+biogrid.main<-biogrid[,c("Hugo.A","SYNB"),with=F]
+setnames(biogrid.main,c("A","B"))
+setkey(biogrid.main)
+biogrid.main<-unique(biogrid.main)
+
+biogrid.comp<-biogrid[,c("Hugo.B","SYNA"),with=F]
 setnames(biogrid.comp,c("A","B"))
+setkey(biogrid.comp)
+biogrid.comp<-unique(biogrid.comp)
 
-biogrid<-unique(rbind(biogrid,biogrid.comp))
-
-length(unique(intersect(TCGA.MUT.45.MID.2$Hugo_Symbol, biogrid$Hugo.A)))
+biogrid.comb<-unique(rbind(biogrid.main,biogrid.comp))
 
 #Get degree
-biogrid.degree<-biogrid[,list(degree=length(B)),by="A"]
+biogrid.degree<-biogrid.comb[,list(degree=length(B)),by="A"]
 biogrid.degree<-biogrid.degree[order(degree, decreasing=T),]
 setnames(biogrid.degree, c("Hugo_Symbol", "degree"))
 biogrid.degree$DEGREE.CUT<-cut(biogrid.degree$degree, quantile(biogrid.degree$degree, c(0,0.25,0.50,0.75,1)), include.lowest=T)
+
+length(unique(as.vector(biogrid.degree$Hugo_Symbol)))
+length(unique(intersect(TCGA.MUT.45.MID.2$Hugo_Symbol, biogrid.degree$Hugo_Symbol)))
