8e7091cddacd8a12c0e21a2ea8647b05e14ec802
LOBUTO <jzamalloa1@gmail.com>
2015-01-21 02:58:04 -0500

    added function to calculate p value per metabolite based on randomized simulations

--- Function.DIFF.EXP.MR.R
+++ Function.DIFF.EXP.MR.R
@@ -7,10 +7,10 @@ library(data.table)
 
 args<-commandArgs(trailingOnly=T) 
 
-maf.in<-args[1] #TCGA maf file 
-mr.in<-readRDS(args[2]) #Target mr
+maf.in<-args[1] #TCGA maf file used in mr.in build!!!
+mr.in<-readRDS(args[2]) #Target mr as in 012115.MET.ENRICHED.FILTERED.rds
 exp.in<-readRDS(args[3]) #Expression object of the form 102514.CANCER.MATRICES.NORMALIZED.OBJ.rds
-cancer<-args[4] #Type of cancer to be analyzed
+cancer<-args[4] #Type of cancer to be analyzed (i.e. "BRCA")
 output.file<-args[5]
 
 #######FUNCTIONS#######
@@ -34,8 +34,9 @@ Function.Process.MAF<-function(maf.file){
   return(maf.record)
 }
 
-Function.RNAseq.Differential.Expression.V2<-function(normalized.matrices.object, target.cancer.samples) {
+Function.RNAseq.Differential.Expression.V3<-function(normalized.matrices.object, target.cancer.samples) {
   #Takes object from function Function.RNAseq.Matrices.Normalization() and target cancer samples to perform differential expression
+  #MODIFIED: To count number of differentially expressed gene at adj.p.val<0.05
   
   require(limma)
   require(edgeR)
@@ -65,8 +66,11 @@ Function.RNAseq.Differential.Expression.V2<-function(normalized.matrices.object,
     all.G.fit<-as.data.table(all.G.fit)
   }
   
+  #Count differentially expressed genes
+  n.diff<-nrow(all.G.fit[adj.P.Val<0.05,])
+  
   #Return topTable
-  return(all.G.fit)
+  return(n.diff)
 }
 
 Function.MR.Process<-function(mr.in, cancer){
@@ -82,28 +86,30 @@ Function.Main<-function(main.table, maf.table, exp.in){
   #Calculates number of differentially expressed genes per metabolite
   
   internal.function<-function(gene){
-    samples<-as.vector(maf.table[Hugo_Symbol %in% gene,]$PATIENT)
-    not.samples<-as.vector(maf.table[!(Hugo_Symbol %in% gene),]$PATIENT)
+    samples<-unique(as.vector(maf.table[Hugo_Symbol %in% gene,]$PATIENT))
+    not.samples<-unique(as.vector(maf.table[!(PATIENT %in% samples),]$PATIENT))
     
     #Check that we have at least one sample to do differential expression
-    if length(samples)>1{
+    if (length(samples)>1){
       
-      #Do differential expression
-      diff.exp<-Function.RNAseq.Differential.Expression.V2(exp.in, samples)
-      diff.exp.not<-Function.RNAseq.Differential.Expression.V2(exp.in, not.samples)
+      #Do differential expression to obtain number of diff expression
+      diff.exp<-Function.RNAseq.Differential.Expression.V3(exp.in, samples)
+      diff.exp.not<-Function.RNAseq.Differential.Expression.V3(exp.in, not.samples)
       
-      #Count differentially expressed genes
-      n.diff<-nrow(diff.exp[adj.P.Val<0.05,])
-      n.not.diff<-nrow(diff.exp.not[adj.P.Val<0.05,])
+      #Count samples
+      n.samples<-length(samples)
+      n.not.samples<-length(not.samples)
       
     } else {
       #If only one sample then experiment will be inconclusive
-      n.diff<-NA
-      n.not.diff<-NA
+      diff.exp<-NA
+      diff.exp.not<-NA
+      n.samples<-NA
+      n.not.samples<-NA
     }
     
     #Return number of differentially expressed genes
-    return(list(N.DIFF=n.diff, N.NOT.DIFF=n.not.diff))
+    return(list(N.DIFF=diff.exp, N.NOT.DIFF=diff.exp.not, N.SAMPLES=n.samples, N.NOT.SAMPLES=n.not.samples))
   }
   
   #Execute comparisson of metabolite related gene differential expression vs rest
@@ -113,4 +119,41 @@ Function.Main<-function(main.table, maf.table, exp.in){
   return(main.table)
 }
 
-#######EXECUTE########
+Function.Main.PVAL<-function(main.command, maf.table,exp.in){
+  #Calculates p-values per metabolite in main.command run
+  
+  #Function for simulation 
+  internal.function<-function(n.samples,n.diff, n.not.samples, n.not.diff){
+    
+    n.diff.pval<-mean(replicate(100, Function.RNAseq.Differential.Expression.V3(exp.in, sample(all.samples, n.samples))) >=n.diff)
+    n.not.diff.pval<-mean(replicate(100, Function.RNAseq.Differential.Expression.V3(exp.in, sample(all.samples, n.not.samples))) >=n.not.diff)
+    
+    return(list(N.DIFF.PVAL=n.diff.pval, N.NOT.DIFF.PVAL=n.not.diff.pval))
+  }
+  
+  #Exectute
+  all.samples<-unique(as.vector(maf.table$PATIENT))
+  main.table<-main.command[,internal.function(N.SAMPLES, N.DIFF, N.NOT.SAMPLES, N.NOT.DIFF),by=c("METABOLITE","KEGG_ID")]
+  
+  #Clean up and return
+  main.table<-main.table[order(N.DIFF.PVAL),]
+  return(main.table)
+}
+
+#######EXECUTION########
+maf.table<-Function.Process.MAF(maf.in)
+cat ("Processed maf file\n")
+
+main.table<-Function.MR.Process(mr.in, cancer)
+cat ("Processed mr file\n")
+
+cat ("Performing expression analysis\n")
+main.command<-Function.Main(main.table, maf.table, exp.in)
+
+cat ("Obtaining p values (Simmulations) \n")
+main.command.pval<-Function.Main.PVAL(main.command, maf.table, exp.in)
+
+######OUTPUT#########
+write.table(file=output.file, main.command.pval, sep="\t", quote=F, row.names=F, col.names=T)
+cat ("DONE")
+
