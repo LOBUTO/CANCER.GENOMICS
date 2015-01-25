90f9b6049474ed9489ffb1bc949e3c900cba726d
LOBUTO <jzamalloa1@gmail.com>
2015-01-24 21:32:34 -0500

    fixed reading tables from V1

--- Function.Exp.Normal.Matrix.R
+++ Function.Exp.Normal.Matrix.R
@@ -129,9 +129,9 @@ Function.RNAseq.Matrices.Normalization<-function(normal.matrix, cancer.matrix, r
   normal.matrix$rn<-rownames(normal.matrix)
   dummy.expression.matrix<-join_all(list(as.data.frame(cancer.matrix), as.data.frame(normal.matrix)), by="rn", type="inner")
   
-  print (length(rownames(dummy.expression.matrix)))
-  print (rownames(dummy.expression.matrix)[duplicated(rownames(dummy.expression.matrix))])
-  print (length(rownames(dummy.expression.matrix)[!duplicated(rownames(dummy.expression.matrix))]))
+  print (length(dummy.expression.matrix$rn))
+  print (dummy.expression.matrix$rn[duplicated(dummy.expression.matrix$rn)])
+  print (length(dummy.expression.matrix$rn[!duplicated(dummy.expression.matrix$rn)]))
   
   rownames(dummy.expression.matrix)<-dummy.expression.matrix$rn
   dummy.expression.matrix$rn<-NULL #Remove column used to combine data frames
