aa596a1242fe3eba6a147cb7118c9b2140eb1308
LOBUTO <jzamalloa1@gmail.com>
2015-03-18 04:48:47 -0400

    added filtering of low counts and nt.length as features

--- Function.DEEP.LEARNING.1.R
+++ Function.DEEP.LEARNING.1.R
@@ -43,24 +43,39 @@ Function.PROCESS.ANNOVAR<-function(MAF.ANNOVAR){
   return(MAF.ANNOVAR)
 }
 
-Function.PREP.MAF.CLASS.ML<-function(MAF.ANNOVAR, REP.TIME, HUGO=F){
+Function.PREP.MAF.CLASS.ML<-function(MAF.ANNOVAR, REP.TIME, NT.LENGTH, EXTRA=c(), FILTER=F){
   #Added replication time and rep class
   
   require(data.table)
   
+  normalize.vector<-function(x){
+    y=(x-min(x))/(max(x)- min(x))
+    return(y)
+  }
+  
+  #Filter out low counts?
+  if (FILTER==T){
+    MIN.MAF<-min(MAF.ANNOVAR$MAF[MAF.ANNOVAR$MAF!=0])
+    MAF.ANNOVAR<-MAF.ANNOVAR[MAF>MIN.MAF,]
+  }
+  
   #Enter rep info
   MAF.ANNOVAR<-merge(MAF.ANNOVAR, REP.TIME, by="Hugo_Symbol")
+  
+  #Enter length info
+  MAF.ANNOVAR<-merge(MAF.ANNOVAR, NT.LENGTH, by="Hugo_Symbol")
+  MAF.ANNOVAR$NT<-normalize.vector(MAF.ANNOVAR$NT)
   
   #Classify MAF into categories
   MAF.ANNOVAR$MAF.CUT<-cut(MAF.ANNOVAR$MAF, quantile(MAF.ANNOVAR$MAF, c(0,0.33,0.66,1)), include.lowest=T, 
                            labels=as.factor(c("low", "medium", "high")))
   
-  #Filter out not needed columns and place classificaiton column at the end - Removing melting temperature
-  if (HUGO==F){
-    MAF.ANNOVAR<-MAF.ANNOVAR[,c("TYPE", "Chrom", "PHAST", "REF.ALT","REP.CLASS", "TRIMER", "MAF.CUT", "REP.TIME"), with=F]  
-  } else{
-    MAF.ANNOVAR<-MAF.ANNOVAR[,c("TYPE", "Hugo_Symbol", "Chrom", "PHAST", "REF.ALT","REP.CLASS", "TRIMER", "MAF.CUT", "REP.TIME"), with=F]  
-  }
+  #Get cummulative MAF per hugo
+  MAF.ANNOVAR[,HUGO.MAF.SUM:=sum(MAF), by="Hugo_Symbol"]
+  
+  #Filter out not needed columns and place classificaiton column at the end
+  MAF.ANNOVAR<-MAF.ANNOVAR[,c("TYPE", "Chrom", "PHAST", "REF.ALT","REP.CLASS", "TRIMER", "MT", "MAF.CUT", "REP.TIME", "HUGO.MAF.SUM", "NT",
+                              EXTRA), with=F]  
   
   #Return
   return(MAF.ANNOVAR)
@@ -70,7 +85,8 @@ args<-commandArgs(trailingOnly=T)
 
 maf.annovar<-args[1] #Such as 122114.THOUSAND.SNP.TRIMERS.PLUS.PHAST
 chen.rep<-args[2] #Such as 123114.BRCA.MAF.TRIMERS.csv
-output.file<-args[3]
+exon.nt<-args[3]
+output.file<-args[4]
 
 print ("Processing annovar")
 MAF.ANNOVAR<-fread(maf.annovar, drop=c(1,6),sep="\t", header=F, stringsAsFactors=F)
@@ -78,13 +94,16 @@ MAF.ANNOVAR<-Function.PROCESS.ANNOVAR(MAF.ANNOVAR)
 
 CHEN.REP<-fread(chen.rep, header=T, sep="\t",stringsAsFactors=F, drop=2)
 
+EXON.NT<-fread(exon.nt, header=T, sep="\t", stringsAsFactors=F, drop=2:6)
+EXON.NT<-EXON.NT[,list(NT=sum(FEAT_END-FEAT_START)), by=Hugo_Symbol]
+
 MAF.ANNOVAR.CLASS<-Function.PREP.MAF.CLASS.ML(MAF.ANNOVAR,  CHEN.REP)
 
 ######## Predicting with H2O on classifiable MAF variable#########################
 print ("building annovar class")
-MAF.ANNOVAR.CLASS<-Function.PREP.MAF.CLASS.ML(MAF.ANNOVAR,  CHEN.REP)
+MAF.ANNOVAR.CLASS<-Function.PREP.MAF.CLASS.ML(MAF.ANNOVAR,  CHEN.REP, EXON.NT,"MAF", FILTER=T)
 
-# Start a local cluster with 8GB RAM
+# Start a local cluster with 30GB RAM
 print ("setting up h2o")
 localH2O = h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, max_mem_size= '30g', nthreads=-1) 
 
@@ -103,9 +122,9 @@ TEST_MAF<-as.factor(MAF.ANNOVAR.CLASS$MAF.CUT[TEST_ROWS])
 
 #Model without dropout
 print ("building model")
-MAF.MODEL<-h2o.deeplearning(x=c(1:6), y=7, data=h2o_maf[TRAIN_ROWS[1:100000],],
-                            activation = "Tanh", balance_classes = TRUE, hidden = c(1000,500,50), epochs = 400)
+MAF.MODEL<-h2o.deeplearning(x=c(1:4,6,7,9,11), y=8, data=h2o_maf[TRAIN_ROWS[1:10000],],
+                            activation = "Tanh", balance_classes = TRUE, hidden = c(100,100), epochs = 300)
 print ("done with dnn")
 #Save model
-h2o.saveModel(MAF.MODEL, "RESULTS", "MAF.MODEL.100000.3.1000.500.50.E.400.REP.CLASS.h2o")
+h2o.saveModel(MAF.MODEL, "RESULTS", "MAF.MODEL.FILTERED.NT.REP.TIME.10000.2.100.100.E.300.h2o")
 print ("done saving")
