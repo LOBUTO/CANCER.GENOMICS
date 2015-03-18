5cd59f7bac22a92ba100b8e62f337b96737f9a3b
LOBUTO <jzamalloa1@gmail.com>
2015-03-18 05:14:30 -0400

    added filtering of low counts and nt.length as features

--- Function.DEEP.LEARNING.1.R
+++ Function.DEEP.LEARNING.1.R
@@ -63,8 +63,6 @@ Function.PREP.MAF.CLASS.ML<-function(MAF.ANNOVAR, REP.TIME, NT.LENGTH, EXTRA=c()
   MAF.ANNOVAR<-merge(MAF.ANNOVAR, REP.TIME, by="Hugo_Symbol")
   
   #Enter length info
-  print (MAF.ANNOVAR)
-  print(NT.LENGTH)
   MAF.ANNOVAR<-merge(MAF.ANNOVAR, NT.LENGTH, by="Hugo_Symbol")
   MAF.ANNOVAR$NT<-normalize.vector(MAF.ANNOVAR$NT)
   
@@ -98,9 +96,6 @@ CHEN.REP<-fread(chen.rep, header=T, sep="\t",stringsAsFactors=F, drop=2)
 
 EXON.NT<-fread(exon.nt, header=T, sep="\t", stringsAsFactors=F, drop=2:6)
 EXON.NT<-EXON.NT[,list(NT=sum(FEAT_END-FEAT_START)), by=Hugo_Symbol]
-print(EXON.NT)
-
-MAF.ANNOVAR.CLASS<-Function.PREP.MAF.CLASS.ML(MAF.ANNOVAR,  CHEN.REP)
 
 ######## Predicting with H2O on classifiable MAF variable#########################
 print ("building annovar class")
