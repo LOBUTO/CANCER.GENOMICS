77c5dd8b20138aa4144a16ee3a491b17b324c9a3
LOBUTO <jzamalloa1@gmail.com>
2015-03-18 01:50:16 -0400

    training with 60% instead

--- Function.DEEP.LEARNING.1.R
+++ Function.DEEP.LEARNING.1.R
@@ -103,8 +103,8 @@ TEST_MAF<-as.factor(MAF.ANNOVAR.CLASS$MAF.CUT[TEST_ROWS])
 #Model without dropout
 print ("building model")
 MAF.MODEL<-h2o.deeplearning(x=c(1:6), y=7, data=h2o_maf[TRAIN_ROWS[1:10000],],
-                            activation = "Tanh", balance_classes = TRUE, hidden = c(1000,500,100), epochs = 400)
+                            activation = "Tanh", balance_classes = TRUE, hidden = c(100,50,10), epochs = 500)
 print ("done with dnn")
 #Save model
-h2o.saveModel(MAF.MODEL, "RESULTS", "MAF.MODEL.10000.3.1000.500.100.E.400.REP.CLASS.h2o")
+h2o.saveModel(MAF.MODEL, "RESULTS", "MAF.MODEL.10000.3.100.50.10.E.500.REP.CLASS.h2o")
 print ("done saving")
