8ec1cbbfca8d7461283792d31fec6704fba68417
LOBUTO <jzamalloa1@gmail.com>
2015-05-09 16:25:10 -0400

    function for deep learning of metabolic table obj

---
+++ Function.MET.DEEP.R
@@ -0,0 +1,63 @@
+#Function.MET.DEEP.R
+#050915
+#Calculate prediciton errors of metabolite levels using h2o's deep learning module
+
+library(data.table)
+library(reshape2)
+library(h2o)
+
+#Functions
+Function.Main<-function(met.obj, method){
+  
+  #Load met table
+  MET<-readRDS(met.obj)
+  
+  #Open h2o connection
+  localH2O = h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, max_mem_size= '32g', nthreads=-1) 
+  
+  #Introduce met obj as h2o object
+  key.v<-sample(letters,1)
+  h2o_MET<-as.h2o(localH2O, MET, key=key.v) 
+  
+  #Introduce parameters for training
+  DEEP.MET<-data.table()
+  METHOD=method
+  HIDDEN<-c(80,40,10)
+  INPUT.DR<-0
+  HIDDEN.DR<-c(0.5,0.5,0.5)
+  FEATURES<-"ALL.RECON.EXP"
+  
+  for (n in 1:10) {
+    
+    #Model with/out dropout
+    print (c("building model:",n))
+    MODEL.MET<-h2o.deeplearning(x=5:ncol(MET), y=2, data=h2o_MET, classification = F, nfolds = 5,
+                                activation = METHOD, balance_classes = TRUE, hidden = HIDDEN, epochs = 500,
+                                input_dropout_ratio = INPUT.DR , hidden_dropout_ratios = HIDDEN.DR)
+    
+    TRAIN.ERROR<-MODEL.MET@model$train_sqr_error
+    TEST.ERROR<-MODEL.MET@model$valid_sqr_error
+    
+    CUR.PRED<-data.table(ITER=n, TRAIN.ACC=1-sqrt(TRAIN.ERROR), TEST.ACC=1-sqrt(TEST.ERROR), METHOD=METHOD, FEATURES=FEATURES,
+                         HIDDEN=paste(HIDDEN,collapse = "."), INPUT.DR=INPUT.DR, HIDDEN.DR=paste(HIDDEN.DR,collapse="."))  
+    
+    #Assign predictors
+    DEEP.MET<-rbind(DEEP.MET,CUR.PRED)
+  }
+  
+  #Return
+  return(DEEP.MET)
+}
+
+#Arguments
+args<-commandArgs(trailingOnly=T)
+met.obj<-args[1]
+output.file<-args[2]
+print("opened files")
+
+#Execute
+main.obj<-Function.Main(met.obj, method="TanhWithDropout")
+
+#Write out
+saveRDS(main.obj, output.file)
+print ("done writing to output")
