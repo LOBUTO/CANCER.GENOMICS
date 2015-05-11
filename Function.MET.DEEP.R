a88cdd96c9e0044a2f30a3ebb6f9901a7b8a6f2b
LOBUTO <jzamalloa1@gmail.com>
2015-05-11 15:15:34 -0400

    added artificial binary classification function after regression

--- Function.MET.DEEP.R
+++ Function.MET.DEEP.R
@@ -5,6 +5,7 @@
 library(data.table)
 library(reshape2)
 library(h2o)
+library(caret)
 
 #Functions
 Function.Main<-function(met.obj, method, hidden, hidden.dr, input.dr){
@@ -27,7 +28,7 @@ Function.Main<-function(met.obj, method, hidden, hidden.dr, input.dr){
   HIDDEN.DR<-1/as.numeric(unlist(strsplit(hidden.dr,"[.]")))
   FEATURES<-"ALL.RECON.EXP"
   
-  for (n in c(1)) {
+  for (n in 1:10) {
     
     #Model with/out dropout
     print (c("building model:",n))
@@ -57,6 +58,71 @@ Function.Main<-function(met.obj, method, hidden, hidden.dr, input.dr){
   return(DEEP.MET)
 }
 
+Function.Main.Bin<-function(met.obj, method, hidden, hidden.dr, input.dr){
+  
+  
+  #Load met table
+  MET<-readRDS(met.obj)
+  
+  #Open h2o connection
+  localH2O = h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, max_mem_size= '32g', nthreads=-1) 
+  
+  #Introduce met obj as h2o object
+  key.v<-sample(letters,1)
+  h2o_MET<-as.h2o(localH2O, MET, key=key.v) 
+  
+  #Introduce parameters for training
+  DEEP.MET<-data.table()
+  METHOD=method
+  HIDDEN<-as.numeric(unlist(strsplit(hidden,"[.]")))
+  INPUT.DR<-as.numeric(input.dr)
+  HIDDEN.DR<-1/as.numeric(unlist(strsplit(hidden.dr,"[.]")))
+  FEATURES<-"ALL.RECON.EXP.BIN"
+  
+  #Break into equal representative folds of 1/0 dysregulated cohorts
+  x<-ifelse(MET$MET>2, 1, 0)
+  RAND_FOLDS.1<-createFolds(which(x==1),5)
+  RAND_FOLDS.0<-createFolds(which(x==0),5)
+  TEST_ROWS<-c( which(x==1)[RAND_FOLDS.1[[1]]], which(x==0)[RAND_FOLDS.0[[1]]])
+  TRAIN_ROWS<-setdiff(1:nrow(MET), TEST_ROWS)
+  
+  for (n in 1:10) {
+    
+    #Model with/out dropout
+    print (c("building model",n))
+    
+    if (METHOD=="TanhWithDropout"){
+      MODEL.MET<-h2o.deeplearning(x=5:ncol(MET), y=2, data=h2o_MET, classification = F, nfolds = 5,
+                                  activation = METHOD, balance_classes = TRUE, hidden = HIDDEN, epochs = 500,
+                                  input_dropout_ratio = INPUT.DR , hidden_dropout_ratios = HIDDEN.DR)  
+    } else {
+      MODEL.MET<-h2o.deeplearning(x=5:ncol(MET), y=2, data=h2o_2HG[TRAIN_ROWS,], classification = F, nfolds = 5,
+                                  activation = METHOD, balance_classes = TRUE, hidden = HIDDEN, epochs = 500)  
+    }
+    
+    TRAIN.ERROR<-MODEL.MET@model$train_sqr_error
+    TEST.ERROR<-MODEL.MET@model$valid_sqr_error
+    
+    TRAIN.ACC<-1-sqrt(TRAIN.ERROR)/(max(MET$MET)- min(MET$MET))
+    TEST.ACC<-1-sqrt(TEST.ERROR)/(max(MET$MET)- min(MET$MET))
+    
+    TEST.SAMPLE<-MET[TEST_ROWS, c("SAMPLE", "MET"), with=F]
+    TEST.SAMPLE$BIN.MET<-ifelse(TEST.SAMPLE$MET>2, 1, 0)
+    TEST.SAMPLE$PREDICT<-as.numeric(as.matrix(h2o.predict(MODEL.2HG, h2o_2HG[TEST_ROWS,])$predict))
+    TEST.SAMPLE$BIN.PRED<-ifelse(TEST.SAMPLE$PREDICT>2, 1, 0)
+    VALID.ACC<-mean(TEST.SAMPLE$BIN.MET==TEST.SAMPLE$BIN.PRED)
+    
+    CUR.PRED<-data.table(TRAIN.ACC=TRAIN.ACC, TEST.ACC=TEST.ACC, VALID.ACC, METHOD=METHOD, FEATURES=FEATURES,
+                         HIDDEN=paste(HIDDEN,collapse="."), INPUT.DR=INPUT.DR, HIDDEN.DR=paste(HIDDEN.DR,collapse="."))  
+    
+    #Assign predictors
+    DEEP.MET<-rbind(DEEP.MET, CUR.PRED)
+  }
+  
+  #Return
+  return(DEEP.MET)
+}
+
 #Arguments
 args<-commandArgs(trailingOnly=T)
 met.obj<-args[1]
@@ -68,7 +134,7 @@ method=args[6] #choose between "Tanh" or ""TanhWithDropout"
 print("opened files")
 
 #Execute
-main.obj<-Function.Main(met.obj, method=method, hidden=hidden, hidden.dr=hidden.dr, input.dr=input.dr)
+main.obj<-Function.Main.Bin(met.obj, method=method, hidden=hidden, hidden.dr=hidden.dr, input.dr=input.dr)
 
 #Write out
 saveRDS(main.obj, output.file)
