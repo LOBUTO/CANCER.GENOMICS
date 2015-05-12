daf296c279939f09dbb6e5659ec32ec1b676e6b0
LOBUTO <jzamalloa1@gmail.com>
2015-05-12 00:36:35 -0400

    corrected for balancing of classes prior to classification algorithm, added replicative of x-folds across multiple test sizes, added all onto new function Function.Main.Class

--- Function.MET.DEEP.R
+++ Function.MET.DEEP.R
@@ -123,6 +123,72 @@ Function.Main.Bin<-function(met.obj, method, hidden, hidden.dr, input.dr){
   return(DEEP.MET)
 }
 
+Function.Main.Class<-function(met.obj, method, hidden, hidden.dr, input.dr){
+  
+  #Load met table
+  MET<-readRDS(met.obj)
+  
+  #Open h2o connection
+  localH2O = h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, max_mem_size= '32g', nthreads=-1) 
+  
+  #Introduce met obj as h2o object
+  key.v<-sample(letters,1)
+  h2o_MET<-as.h2o(localH2O, MET, key=key.v) 
+  
+  #Introduce parameters for training
+  DEEP.MET<-data.table()
+  METHOD=method
+  HIDDEN<-as.numeric(unlist(strsplit(hidden,"[.]")))
+  INPUT.DR<-as.numeric(input.dr)
+  HIDDEN.DR<-1/as.numeric(unlist(strsplit(hidden.dr,"[.]")))
+  FEATURES<-c(7,10,20,50,100,200,400,800,ncol(MET))
+  
+  #Distribute into folds evenly for cross-validation
+  RAND_FOLDS.1<-createFolds(which(MET$MET==1),5)
+  RAND_FOLDS.0<-createFolds(which(MET$MET==0),5)
+  
+  for (f in FEATURES){
+    
+    for (r in 1:5){
+      
+      TEST_ROWS<-c( which(MET$MET==1)[RAND_FOLDS.1[[r]]], which(MET$MET==0)[RAND_FOLDS.0[[r]]])
+      TRAIN_ROWS<-setdiff(1:nrow(MET), TEST_ROWS)
+      
+      for (n in 1:10) {
+        
+        print (c("building model", f, r, n))
+        
+        #Model with/out dropout
+        if (METHOD=="TanhWithDropout"){
+          MODEL.MET<-h2o.deeplearning(x=5:f, y=2, data=h2o_MET[TRAIN_ROWS,], classification = T,
+                                      activation = METHOD, balance_classes = TRUE, hidden = HIDDEN, epochs = 500,
+                                      input_dropout_ratio = INPUT.DR , hidden_dropout_ratios = HIDDEN.DR)  
+        } else {
+          MODEL.MET<-h2o.deeplearning(x=5:f, y=2, data=h2o_MET[TRAIN_ROWS,], classification = T, 
+                                      activation = METHOD, balance_classes = TRUE, hidden = HIDDEN, epochs = 500)  
+        }
+        
+        PREDICT.MET.TRAIN<-h2o.predict(MODEL.MET, h2o_MET[TRAIN_ROWS,])$predict
+        PREDICT.MET.TRAIN<-as.factor(as.matrix(PREDICT.MET.TRAIN))
+        PREDICT.MET.TEST<-h2o.predict(MODEL.MET, h2o_MET[TEST_ROWS,])$predict
+        PREDICT.MET.TEST<-as.factor(as.matrix(PREDICT.MET.TEST))
+        
+        TRAIN.ACC<-round(confusionMatrix(PREDICT.MET.TRAIN, MAIN.MET.EXP$MET[TRAIN_ROWS])$overall[1],4)
+        TEST.ACC<-round(confusionMatrix(PREDICT.MET.TEST, MAIN.MET.EXP$MET[TEST_ROWS])$overall[1],4)  
+        
+        CUR.PRED<-data.table(TRAIN.ACC=TRAIN.ACC, TEST.ACC=TEST.ACC, ITER=n,  FOLD=r, FEATURES=f-5+1,  METHOD=METHOD,
+                             HIDDEN=paste(HIDDEN,collapse="."), INPUT.DR=INPUT.DR, HIDDEN.DR=paste(HIDDEN.DR,collapse="."))  
+        
+        #Assign predictors
+        DEEP.MET<-rbind(DEEP.MET,CUR.PRED)
+      }
+    }
+  }
+  
+  #Return
+  return(DEEP.MET)
+}
+
 #Arguments
 args<-commandArgs(trailingOnly=T)
 met.obj<-args[1]
@@ -134,7 +200,7 @@ method=args[6] #choose between "Tanh" or ""TanhWithDropout"
 print("opened files")
 
 #Execute
-main.obj<-Function.Main.Bin(met.obj, method=method, hidden=hidden, hidden.dr=hidden.dr, input.dr=input.dr)
+main.obj<-Function.Main.Class(met.obj, method=method, hidden=hidden, hidden.dr=hidden.dr, input.dr=input.dr)
 
 #Write out
 saveRDS(main.obj, output.file)
