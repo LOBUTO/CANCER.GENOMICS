e501fa66da8e9ae5ca3c9c4861364d420c77a696
LOBUTO <jzamalloa1@gmail.com>
2015-06-25 09:55:04 -0400

    minor correction

--- Function.HOO.ROC.OPTIM.R
+++ Function.HOO.ROC.OPTIM.R
@@ -176,7 +176,7 @@ degree.filters<-c(50,60,70,80,90,100,150,250,300)
 MAIN.OBJ<-data.table()
 for (d.f in d.filters){
   for (deg in degree.filters){
-    print (c(d, degree.filters))
+    print (c(d.f, degree.filters))
     TEMP.ROC<-Function.Main.HOO.ROC(kegg.path, recon.directed, path.filter.l=3, path.filter.r=60, d=d.f, degree.filter=deg)
     TEMP.ROC$d<-d.f
     TEMP.ROC$DEGREE.FILTER<-deg
