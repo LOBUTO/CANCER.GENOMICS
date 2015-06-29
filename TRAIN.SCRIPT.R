40217fb2c8bf6d2beefa8ddb8279c49310ecb9cc
LOBUTO <jzamalloa1@gmail.com>
2015-06-28 23:13:57 -0400

    fixed errors, testing with ten percent

--- TRAIN.SCRIPT.R
+++ TRAIN.SCRIPT.R
@@ -21,7 +21,7 @@ DEEP.2HG<-data.table()
 
 #No dropout model
 print ("modeling without dropout")
-MODEL.2HG<-h2o.deeplearning(x=2:785, y=1, data=train.h2o, classification = T, nfolds = 10,
+MODEL.2HG<-h2o.deeplearning(x=2:785, y=1, data=train.h2o[1:4200,], classification = T, nfolds = 10,
                             activation = "Tanh", balance_classes = TRUE, hidden = c(800,800,800), epochs = 500)
 
 DEEP.2HG<-rbind(DEEP.2HG, data.table(METHOD="Tanh", INPUT.DR=0.5, HIDDEN="800.800.800", HIDDEN.DR=0,
@@ -40,13 +40,13 @@ for (id in INPUT.DR){
   #Model with/out dropout
   print (c("building model", id))
   
-  MODEL.2HG<-h2o.deeplearning(x=2:785, y=1, data=train.h2o, classification = T, nfolds = 10,
+  MODEL.2HG<-h2o.deeplearning(x=2:785, y=1, data=train.h2o[1:4200,], classification = T, nfolds = 10,
                               activation = METHOD, balance_classes = TRUE, hidden = HIDDEN, epochs = 500,
                               input_dropout_ratio = id , hidden_dropout_ratios =HIDDEN.DR)
   
   CUR.PRED<-data.table(METHOD=METHOD, INPUT.DR=id,  HIDDEN=paste(HIDDEN, collapse="."), HIDDEN.DR=paste(HIDDEN.DR,collapse="."),
                        TRAIN.CLASS.ERROR=MODEL.2HG@model$train_class_error,
-                       VALID.CLASS.ERROR=MODEL.2HG@model$valid_class_error))
+                       VALID.CLASS.ERROR=MODEL.2HG@model$valid_class_error)
   
   #Assign predictors
   DEEP.2HG<-rbind(DEEP.2HG, CUR.PRED)
