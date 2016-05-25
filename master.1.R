07f4604e369aed7b0bdb55b3f0d74ec9332efc0d
jzamalloa <zamalloa@princeton.edu>
2016-05-25 17:51:42 -0400

    updating

--- master.1.R
+++ master.1.R
@@ -13,30 +13,30 @@ for (i in c(0)){
 
     test.table <- tcga.drug.feat$FEAT.TABLE[DRUG %in% target.drug,][SAMPLE %in% target.samples,]
     setnames(test.table, colnames(cgp.cor.AUC))
-    train.table <- nci.cgp.feat.class
+    temp.table <- nci.cgp.feat.class
+    train.rows <- sample(1:nrow(temp.table), nrow(temp.table)*0.8)
+    valid.rows <- setdiff(1:nrow(temp.table), train.rows)
+    train.table <- temp.table[train.rows,]
+    valid.table <- temp.table[train.rows,]
 
     test.table <- test.table[order(cell_name),] #Need to order to get survival curves later!!!
-    #test.table$NORM_AUC <- -test.table$NORM_AUC
-
-    #train.table <- train.table[sample(nrow(train.table)),]
-    #train.table$NORM_AUC <- scale(train.table$NORM_AUC)
-    train.table <- train.table[order(Compound),]
 
     target.drug <- "ALL"
     file.name <- paste0("CGP.AUC_DRUG.TH_", i ,"_",target.drug, "_TARGET",".",iter)
     file.name <- paste0("~/Documents/FOLDER/TABLES/TCGA.TRAINING/", file.name)
 
+    write.table(test.table, paste0(file.name, ".VALID", ".1"), sep = "\t", quote = F, row.names = F, col.names = T )
     write.table(test.table, paste0(file.name, ".TEST", ".1"), sep = "\t", quote = F, row.names = F, col.names = T )
 
     #Write indices
-    #     batch.size <- 100
-    #     train.index <- rep(batch.size, nrow(train.table)%/%batch.size)
-    #
-    #     l.batch <- nrow(train.table)%%batch.size
-    #     if (l.batch>40){
-    #       train.index <- c(train.index, l.batch)
-    #     }
-    train.index <- as.vector(table(train.table$Compound))
+    batch.size <- 100
+    train.index <- rep(batch.size, nrow(train.table)%/%batch.size)
+
+    l.batch <- nrow(train.table)%%batch.size
+    if (l.batch>80){
+      train.index <- c(train.index, l.batch)
+    }
+    #train.index <- as.vector(table(train.table$Compound))
     write.table(train.table, paste0(file.name, ".TRAIN", ".1"), sep = "\t", quote = F, row.names = F, col.names = T )
     write.table(train.index, paste0(file.name, ".TRAIN.INDEX.1"), row.names = F, quote = F, col.names = F)
 
