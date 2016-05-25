a205b2081d676c05f01a8e82e2638ef9a622887a
jzamalloa <zamalloa@princeton.edu>
2016-05-25 18:11:07 -0400

    updating

--- master.1.R
+++ master.1.R
@@ -17,11 +17,12 @@ for (i in c(0)){
 
     test.table <- tcga.drug.feat$FEAT.TABLE[SAMPLE %in% target.samples,]
     setnames(test.table, colnames(cgp.cor.AUC))
+
     temp.table <- nci.cgp.feat.class
     train.rows <- sample(1:nrow(temp.table), nrow(temp.table)*0.8)
     valid.rows <- setdiff(1:nrow(temp.table), train.rows)
     train.table <- temp.table[train.rows,]
-    valid.table <- temp.table[train.rows,]
+    valid.table <- temp.table[valid.rows,]
 
     test.table <- test.table[order(cell_name),] #Need to order to get survival curves later!!!
 
@@ -29,7 +30,7 @@ for (i in c(0)){
     file.name <- paste0("CGP.AUC_DRUG.TH_", i ,"_",target.drug, "_TARGET",".",iter)
     file.name <- paste0("~/Documents/FOLDER/TABLES/TCGA.TRAINING/", file.name)
 
-    write.table(test.table, paste0(file.name, ".VALID", ".1"), sep = "\t", quote = F, row.names = F, col.names = T )
+    write.table(valid.table, paste0(file.name, ".VALID", ".1"), sep = "\t", quote = F, row.names = F, col.names = T )
     write.table(test.table, paste0(file.name, ".TEST", ".1"), sep = "\t", quote = F, row.names = F, col.names = T )
 
     #Write indices
@@ -48,5 +49,6 @@ for (i in c(0)){
     write.table(all.tcga.clinical[SAMPLE %in% test.table$cell_name,][order(SAMPLE),],
                 "~/Documents/FOLDER/TABLES/TCGA.TRAINING/master.clinical.txt",
                 sep="\t", quote=F, row.names = F, col.names=T )
+
   }
 }
