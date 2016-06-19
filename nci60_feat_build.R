9613425002cf78e2cc973e0a5e2f77b8a4e150c1
jzamalloa <zamalloa@princeton.edu>
2016-06-19 14:31:28 -0400

    function to build nci60 prediction table

--- nci60_feat_build.R
+++ nci60_feat_build.R
@@ -84,10 +84,10 @@ Function.build.nci60.feat.class <- function(nci60.exp, nci.gi50, nci60.cgp.drugs
 
 ########################################################################################################################################
 
-OBJ_FOLDER <- "~/Documents/FOLDER/OBJECTS/TCGA.TRAINING/"
+OBJ_FOLDER <- "~/Documents/FOLDER/OBJECTS/"
+TABLES_FOLDER <- "~/Documents/FOLDER/TABLES/TCGA.TRAINING/"
 
-
-nci60.cgp.drugs <- readRDS(paste0(OBJ_FOLDER, "052316.NCI60.HMDB.TABLE.643711.rds"))
+nci60.cgp.drugs <- readRDS(paste0(OBJ_FOLDER, "052316.NCI60.HMDB.TABLE.643711.rds")) #UPDATE as you process
 nci60.exp <- readRDS(paste0(OBJ_FOLDER, "061916.NCI60.EXP.rds"))
 nci.gi50 <- readRDS(paste0(OBJ_FOLDER, "061916.NCI.GI50.rds"))
 
@@ -96,7 +96,7 @@ DRUGS.MET.PROFILE <- readRDS(paste0(OBJ_FOLDER, "061916.DRUGS.MET.PROFILE.rds"))
 cgp.cor.AUC <- readRDS(paste0(OBJ_FOLDER, "052516.CGP.COR.AUC.rds"))
 cgp.exp.matrix <- readRDS(paste0(OBJ_FOLDER, "061716.CGP.EXP.MATRIX.rds"))
 
-nci60.cgp.drugs <- Function.nci60.hmdb.table("DATABASES/CANCER_DATA/NCI.60/NCI60.TC.HMDB.FP4",
+nci60.cgp.drugs <- Function.nci60.hmdb.table(paste0(TABLES_FOLDER,"NCI60.TC.HMDB.FP4"),
                                              DRUGS.MET.PROFILE[DRUG %in% colnames(cgp.cor.AUC),],
                                              nci60.cgp.drugs)
 saveRDS(nci60.cgp.drugs, paste0(OBJ_FOLDER, "052316.NCI60.HMDB.TABLE.PRELAST.rds"))
