1147279597d6d840d3bc17c493e0ef6d5a0fb797
jzamalloa <zamalloa@princeton.edu>
2016-07-08 14:25:00 -0400

    minor fix

--- cgp_model_pred_pca.sh
+++ cgp_model_pred_pca.sh
@@ -6,7 +6,7 @@ python /home/zamalloa/Documents/FOLDER/GIT/cgp_model_pred_pca.py \
 /home/zamalloa/Documents/FOLDER/CGP_FILES/cgp_all_feat_class \
 /home/zamalloa/Documents/FOLDER/TABLES/TCGA.TRAINING/nci60_all_feat_table_scaled_round.3.csv \
 /home/zamalloa/Documents/FOLDER/TABLES/TCGA.TRAINING/PCA.MODELS/nci60.pca.3.random208.pc1000.pkl \
-/home/zamalloa/Documents/FOLDER/RESULTS/TCGA.TRAINING/ALL_nci60_pca700_class_model_700.700.pkl
+/home/zamalloa/Documents/FOLDER/RESULTS/TCGA.TRAINING/PCA.MODELS/ALL_nci60_pca700_class_model_700.700.pkl
 
 Rscript /home/zamalloa/Documents/FOLDER/GIT/cgp.cor.AUC_class_pred_plot.R
 
