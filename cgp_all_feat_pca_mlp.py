3a3310ed3b2b3323b7808dd5c31dc891ba7858e1
jzamalloa <zamalloa@princeton.edu>
2016-07-07 21:01:10 -0400

    minor update

--- cgp_all_feat_pca_mlp.py
+++ cgp_all_feat_pca_mlp.py
@@ -786,10 +786,10 @@ print NEURONS
 for drop_out in [0.5]:
 
     for l in [2]:
-        test_mlp(learning_rate=1.0, L1_reg=0, L2_reg=0.0000000, n_epochs=3000, initial_momentum=0.5, input_p=0.2,
+        test_mlp(learning_rate=10.0, L1_reg=0, L2_reg=0.0000000, n_epochs=3000, initial_momentum=0.5, input_p=0.2,
                      datasets=drugval, train_batch_size=50,
                      n_hidden=input_layers, p=drop_out, dropout=True,
-                     drug_name=TARGET_DRUG +"_cgp_pca300_class_model_" + input_name ,
+                     drug_name=TARGET_DRUG +"_cgp_pca500_class_model_" + input_name ,
                      OUT_FOLDER = OUT_FOLDER)
 
 print "DONE"
