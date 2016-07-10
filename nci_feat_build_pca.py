28d34ed9af7121a9161950d8f69525898c26e83a
jzamalloa <zamalloa@princeton.edu>
2016-07-09 21:24:18 -0400

    minor fixes

--- nci_feat_build_pca.py
+++ nci_feat_build_pca.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import scale
 
 ######################################################################################################
 #LOAD INPUTS
