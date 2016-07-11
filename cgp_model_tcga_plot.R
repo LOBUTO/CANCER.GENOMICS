ab7548a5054d66f856f14d3b867115db82b51974
jzamalloa <zamalloa@princeton.edu>
2016-07-11 18:55:06 -0400

    minor fix

--- cgp_model_tcga_plot.R
+++ cgp_model_tcga_plot.R
@@ -24,7 +24,7 @@ for (pca in c(500, 800, 1000)){
   prediction <- fread(paste0(IN_FOLDER, "cgp_auc_tcga_prediction_", pca), header=T)
 
   #Do we need to filter?
-  prediction <- prediction[LIVED>50, ]
+  prediction <- prediction[ACTUAL>50, ]
 
   prediction[,COUNT:=length(SAMPLE), by="CANCER"]
   prediction <- prediction[COUNT>50,]
