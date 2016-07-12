1195f157152899cb0f9effd772e77e8b2a64656d
jzamalloa <zamalloa@princeton.edu>
2016-07-11 21:42:15 -0400

    minor change

--- cgp_model_tcga_plot.R
+++ cgp_model_tcga_plot.R
@@ -128,7 +128,7 @@ for (pca in c(500, 800, 1000)){
   pred.classes <- lapply(cancers, function(x) {
 
     pred.temp <- prediction[CANCER==x,]
-    pred.temp$CASE <- Function.classify.lived.pred(pred.temp$PREDICTED, sd.multiplier=0.5, effective="POS")
+    pred.temp$CASE <- Function.classify.lived.pred(pred.temp$PREDICTED, sd.multiplier=0.3, effective="POS")
 
     pred.temp <- pred.temp[CASE!="NO_CLASS",]
 
