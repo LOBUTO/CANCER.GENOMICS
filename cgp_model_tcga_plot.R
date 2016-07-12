5be1e45930b286a692af6f6f6b0cc805ba18a42a
jzamalloa <zamalloa@princeton.edu>
2016-07-11 21:29:38 -0400

    minor fix

--- cgp_model_tcga_plot.R
+++ cgp_model_tcga_plot.R
@@ -41,7 +41,7 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
       matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
 
       print(plots[[i]][["TEMP.PLOT"]] +
-            geom_text(aes(plots[[i]][["POS"]], 0.85, label= plots[[i]][["PVAL"]]), size=8.0),
+            geom_text(aes(plots[[i]][["POS"]], 0.85, label= plots[[i]][["PVAL"]]), size=2.0),
             vp = viewport(layout.pos.row = matchidx$row,
                           layout.pos.col = matchidx$col))
     }
