f8110de0aa415bdefb0b0dff75dbfd9c232e9bdc
jzamalloa <zamalloa@princeton.edu>
2016-07-11 21:02:34 -0400

    minor fix

--- cgp_model_tcga_plot.R
+++ cgp_model_tcga_plot.R
@@ -177,7 +177,7 @@ for (pca in c(500, 800, 1000)){
   file.name <- paste0(FIGURES, target.name, pca,  "." , ".survival.pdf")
   pdf(file.name, width=12, height=18)
 
-  multiplot(surv.plot)
+  multiplot(surv.plots)
 
   dev.off()
 
