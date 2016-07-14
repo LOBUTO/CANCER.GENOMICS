db738587c529d8a4bc66b379151a1c2b0f12a257
jzamalloa <zamalloa@princeton.edu>
2016-07-13 21:11:11 -0400

    major fix, understood and implemented standard scaling for new data

--- cgp_pic50_train.R
+++ cgp_pic50_train.R
@@ -17,6 +17,22 @@ valid_rows <- setdiff(all_rows, train_rows)
 train_table <- main.table[train_rows,]
 valid_table <- main.table[valid_rows,]
 
+#Normalizing data with train scales
+train_mean <- apply(train_table[,4:ncol,with=F], 2, mean)
+train_sd   <- apply(train_table[,4:ncol,with=F], 2, sd)
+
+train_table <- cbind(train_table[,1:3,with=F],
+                     sweep(sweep(train_table[,4:ncol,with=F], 2, train_mean, "-"), 2, train_sd, "/")
+                     )
+
+valid_table <- cbind(valid_table[,1:3,with=F],
+                    sweep(sweep(valid_table[,4:ncol,with=F], 2, train_mean, "-"), 2, train_sd, "/")
+                    )
+
+test_table  <- cbind(test_table[,1:3,with=F],
+                    sweep(sweep(test_table[,4:ncol,with=F], 2, train_mean, "-"), 2, train_sd, "/")
+                    )
+
 #Write table
 write.table(train_table, paste0("~/Documents/FOLDER/TABLES/CGP.TRAINING/TRAIN.", target_drug, ".pIC50.csv"),
             sep="\t", quote=F, row.names=F, col.names=T)
