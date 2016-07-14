3b2b819959cc5740fd27dc12c01c697cce85a919
jzamalloa <zamalloa@princeton.edu>
2016-07-13 21:16:31 -0400

    minor fix

--- cgp_pic50_train.R
+++ cgp_pic50_train.R
@@ -7,8 +7,8 @@ target_drug <- "Erlotinib" #For testing
 splits = 0.8
 
 #Split tables
-test_table <- main.table[Compound=="Erlotinib",]
-main.table <- main.table[Compound!="Erlotinib",]
+test_table <- main.table[Compound==target_drug,]
+main.table <- main.table[Compound!=target_drug,]
 
 all_rows <- 1:nrow(main.table)
 train_rows <- sample(all_rows, length(all_rows)*0.8)
@@ -18,19 +18,20 @@ train_table <- main.table[train_rows,]
 valid_table <- main.table[valid_rows,]
 
 #Normalizing data with train scales
-train_mean <- apply(train_table[,4:ncol,with=F], 2, mean)
-train_sd   <- apply(train_table[,4:ncol,with=F], 2, sd)
+cols = ncol(train_table)
+train_mean <- apply(train_table[,4:cols,with=F], 2, mean)
+train_sd   <- apply(train_table[,4:cols,with=F], 2, sd)
 
 train_table <- cbind(train_table[,1:3,with=F],
-                     sweep(sweep(train_table[,4:ncol,with=F], 2, train_mean, "-"), 2, train_sd, "/")
+                     sweep(sweep(train_table[,4:cols,with=F], 2, train_mean, "-"), 2, train_sd, "/")
                      )
 
 valid_table <- cbind(valid_table[,1:3,with=F],
-                    sweep(sweep(valid_table[,4:ncol,with=F], 2, train_mean, "-"), 2, train_sd, "/")
+                    sweep(sweep(valid_table[,4:cols,with=F], 2, train_mean, "-"), 2, train_sd, "/")
                     )
 
 test_table  <- cbind(test_table[,1:3,with=F],
-                    sweep(sweep(test_table[,4:ncol,with=F], 2, train_mean, "-"), 2, train_sd, "/")
+                    sweep(sweep(test_table[,4:cols,with=F], 2, train_mean, "-"), 2, train_sd, "/")
                     )
 
 #Write table
