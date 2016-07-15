2674e515b7d3c302162c644a20b5ba6d17157b61
jzamalloa <zamalloa@princeton.edu>
2016-07-14 20:18:51 -0400

    feature building script translated for effciency to python

---
+++ cgp_pic50_train.py
@@ -0,0 +1,84 @@
+#cgp_pic50_train.py
+import cPickle
+import random
+import sys
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import scale
+import gc
+
+########################################################################################################
+#LOAD FILES
+CGP_FILES = "/home/zamalloa/Documents/FOLDER/CGP_FILES/"
+TRAIN_TABLES = "/home/zamalloa/Documents/FOLDER/TABLES/CGP.TRAINING/"
+
+main_table = pd.read_csv(CGP_FILES + "cgp_cor_pIC50.csv", sep="\t")
+nci_table = pd.read_csv(CGP_FILES + "nci60_cgpfeat_cancertable.csv", sep="\t")
+nci_stats   = pd.read_csv(CGP_FILES + "nci60_stats.csv", sep="\t")
+
+target_drug = sys.argv[1] #For testing
+target_cancers = ["Non-Small Cell Lung"] #EXAMPLE for cells related to Erlotinib (nscl)
+splits = 0.8
+nci_boost = True
+
+print("Done loading files")
+########################################################################################################
+#EXECUTE
+test_table = main_table[main_table.Compound==target_drug]
+target_cells = list(set(test_table.cell_name))
+
+main_table = main_table[main_table.Compound!=target_drug]
+main_table = main_table[main_table.cell_name.isin(target_cells)]
+
+if nci_boost==True:
+
+  nci_cells = list(nci_stats[nci_stats.cancer.isin(target_cancers)]["cell.name"])
+  nci_table = nci_table[nci_table.cell_name.isin(nci_cells)]
+
+  main_table = main_table.append(nci_table, ignore_index=True)
+  main_table.reset_index(drop=True)
+
+gc.collect()
+print("Done combining tables")
+
+#Split tables
+all_rows = xrange(main_table.shape[0])
+train_rows = random.sample(all_rows, int(len(all_rows) * splits))
+valid_rows = list(set(all_rows) - set(train_rows))
+
+main_table = main_table.iloc[:,2:].as_matrix()
+train_table = main_table[train_rows]
+valid_table = main_table[valid_rows]
+
+test_table = test_table.iloc[:,2:].as_matrix()
+
+gc.collect()
+print("Done splitting tables")
+
+#Apply scaling
+std_scale = StandardScaler().fit(train_table)
+train_scaled = std_scale.transform(train_table)
+valid_scaled = std_scale.transform(valid_table)
+test_scaled = std_scale.transform(test_table)
+
+gc.collect()
+
+print("Done executing")
+#####################################################################################################
+#Pickle files
+print(train_scaled.shape)
+print(valid_scaled.shape)
+print(test_scaled.shape)
+
+with open(TRAIN_TABLES + "TRAIN." + target_drug + ".pIC50.pkl", "wb") as tr:
+    cPickle.dump(train_scaled.as_matrix(), tr)
+
+with open(TRAIN_TABLES + "VALID." + target_drug + ".pIC50.pkl", "wb") as vd:
+    cPickle.dump(valid_scaled.as_matrix(), vd)
+
+with open(TRAIN_TABLES + "TEST." + target_drug + ".pIC50.pkl", "wb") as tc:
+    cPickle.dump(test_scaled.as_matrix(), tc)
+
+print("DONE!")
