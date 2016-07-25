be5f4a95b37d1b0796d1c54755810aabc672a207
jzamalloa <zamalloa@princeton.edu>
2016-07-25 13:41:14 -0400

    scripts to build cgp compound specific model training sets

---
+++ cgp_pic50_train_nci.sh
@@ -0,0 +1,10 @@
+#cgp_pic50_train_nci.sh
+#Master script to pre training files to target specific NSC compound
+
+Rscript /tigress/zamalloa/GIT/cgp_pic50_train_nci.R $1
+
+module load cudatoolkit
+module load python
+python /tigress/zamalloa/GIT/cgp_pic50_train.py $1 #Same function as before sufficient?
+
+echo "Done"
