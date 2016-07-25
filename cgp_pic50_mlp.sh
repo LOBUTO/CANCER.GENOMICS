59ee4d17ab5e8c4d58002dc31567e29190bef422
jzamalloa <zamalloa@princeton.edu>
2016-07-25 17:13:42 -0400

    minor fixes

--- cgp_pic50_mlp.sh
+++ cgp_pic50_mlp.sh
@@ -5,9 +5,11 @@
 layers=$1
 
 # Execute mlp
-for drug in A-770041 AMG-706 Axitinib AZD-0530 BIBW2992 \
-                   BMS-536924 Bosutinib Erlotinib FTI-277 Imatinib Lapatinib \
-                   NVP-TAE684 OSI-906 Pazopanib PD-173074 PF-02341066
+# for drug in A-770041 AMG-706 Axitinib AZD-0530 BIBW2992 \
+#                    BMS-536924 Bosutinib Erlotinib FTI-277 Imatinib Lapatinib \
+#                    NVP-TAE684 OSI-906 Pazopanib PD-173074 PF-02341066
+for drug in PHA-665752 PLX4720 SB590885 Sorafenib Sunitinib WH-4-023 A-443654 \
+                BX-795 GDC0941 JW-7-52-1 Midostaurin Rapamycin Temsirolimus
 do
 
   echo $drug
