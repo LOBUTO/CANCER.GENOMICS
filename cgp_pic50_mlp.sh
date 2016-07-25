f771b387b5c2b081cd83b0c054a7da4b10d994a6
jzamalloa <zamalloa@princeton.edu>
2016-07-25 01:04:34 -0400

    minor fixes on mlp bash submission for batch drug

--- cgp_pic50_mlp.sh
+++ cgp_pic50_mlp.sh
@@ -2,13 +2,21 @@
 #cgp_pic50_mlp.sh
 
 # Take arguments
-drug=$1
-layers=$2
+layers=$1
 
 # Execute mlp
-export layers drug
+for drug in A-443654 A-770041 AMG-706 Axitinib AZD-0530 BIBW2992 \
+                   BMS-536924 Bosutinib Erlotinib FTI-277 Imatinib Lapatinib \
+                   NVP-TAE684 OSI-906 Pazopanib PD-173074 PF-02341066
+do
 
-sbatch /tigress/zamalloa/GIT/cgp_pic50_mlp.cmd
+  echo $drug
+  export layers drug
+
+  sbatch /tigress/zamalloa/GIT/cgp_pic50_mlp.cmd
+
+  sleep 1
+done
 
 # Exit
 echo "submitted to sbatch"
