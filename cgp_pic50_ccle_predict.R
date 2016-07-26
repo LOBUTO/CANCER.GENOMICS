ba5bca31f6079fc462618aa634ca9db3d2f58f86
jzamalloa <zamalloa@princeton.edu>
2016-07-26 19:19:01 -0400

    minor fix

--- cgp_pic50_ccle_predict.R
+++ cgp_pic50_ccle_predict.R
@@ -17,13 +17,15 @@ main_table <- main_table[, list(Cor = cor(ACTUAL, PREDICTED)), by=c("cgp_base", 
 
 main_table$based_model <- main_table$cgp_base == main_table$ccle
 
+main_table$has_base    <- main_table$ccle %in% main_table$cgp_base
+
 #Plot
 main_table$layers <- factor(main_table$layers, levels = c("50.50", "100.100", "200.200", "400.400", "500.500"), ordered=T)
 
 pdf(file_out, width=12, height=8)
 
-print (ggplot(main_table, aes(layers, Cor, fill=layers)) + geom_boxplot() + geom_jitter(size=0.5) +
-        facet_wrap(ccle~based_model) + theme_classic() + scale_fill_brewer(palette="Set1") +
+print (ggplot(main_table, aes(layers, Cor, fill=based_model)) + geom_boxplot() + geom_jitter(size=0.5) +
+        facet_grid(~has_base) + theme_classic() + scale_fill_brewer(palette="Set1") +
         theme(axis.text.x = element_text(angle = 45, hjust = 1, size=8)) +
         ggtitle("Prediction on ccle drugs using cgp based model") +
         xlab("MLP architecture") + ylab("Predicted/Actual correlation")
