c9de76fd56e48246bcf095fd4438cbaeff0b3561
jzamalloa <zamalloa@princeton.edu>
2016-07-26 19:01:18 -0400

    script to plot ccle predictions based on cgp models

---
+++ cgp_pic50_ccle_predict.R
@@ -0,0 +1,33 @@
+#cgp_pic50_ccle_predict.R
+#Graph for prediction
+library(data.table)
+library(reshape2)
+library(ggplot2)
+
+####################################################################################
+# LOAD FILES
+main_table <- fread("CGP_FILES/CCLE_RESULTS/ccle_specific_cgp_based_prediction.csv")
+
+file_out <- paste0("FIGURES/CGP.MLP/", as.character(Sys.Date()), "_on_ccle.pdf")
+
+####################################################################################
+# EXECUTE
+main_table <- main_table[, list(Cor = cor(ACTUAL, PREDICTED)), by=c("cgp_base", "ccle", "layers")]
+
+main_table$based_model <- main_table$cgp_base == main_table$ccle
+
+#Plot
+main_table$layers <- factor(main_table$layers, levels = c("50.50", "100.100", "200.200", "400.400", "500.500"), ordered=T)
+
+pdf(file_out, width=12, height=8)
+
+print (ggplot(main_table, aes(layers, Cor, fill=layers)) + geom_boxplot() + geom_jitter(size=0.5) +
+        facet_grid(ccle~based_model) + theme_classic() + scale_fill_brewer(palette="Set1") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1, size=8)) +
+        ggtitle("Prediction on ccle drugs using cgp based model") +
+        xlab("MLP architecture") + ylab("Predicted/Actual correlation")
+        )
+
+dev.off()
+
+print ("Done plotting")
