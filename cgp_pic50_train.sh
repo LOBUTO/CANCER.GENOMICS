4a4e5cae669745eed9bfe88b80be3953aef7e2be
jzamalloa <zamalloa@princeton.edu>
2016-07-26 23:56:44 -0400

    minor fix

--- cgp_pic50_train.sh
+++ cgp_pic50_train.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #cgp_pic50_train.sh
 
+#for drug in 17-AAG AZD6244 Nilotinib PD-0325901 PD-0332991 PLX4720 Erlotinib Lapatinib PHA-665752 Paclitaxel Sorafenib
+for drug in 17-AAG AZD6244 Nilotinib PD-0325901 PD-0332991 Paclitaxel
+do
 
-Rscript /tigress/zamalloa/GIT/cgp_pic50_train.R $1
+  echo $drug
+  Rscript /tigress/zamalloa/GIT/cgp_pic50_train.R $drug
 
-module load cudatoolkit
-module load python
-python /tigress/zamalloa/GIT/cgp_pic50_train.py $1
+  module load cudatoolkit
+  module load python
+  python /tigress/zamalloa/GIT/cgp_pic50_train.py $drug
+
+done
 
 echo "Done"
