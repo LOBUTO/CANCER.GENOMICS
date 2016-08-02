ee47deb4d1b598aa63a8f19bd04919d83172bbe0
jzamalloa <zamalloa@princeton.edu>
2016-08-02 00:22:10 -0400

    minor changes

--- cgp_new_pca.sh
+++ cgp_new_pca.sh
@@ -5,8 +5,8 @@ drug=$1
 m_pca=$2
 g_pca=$3
 
-cgp_new_pca_feat.R $drug $m_pca $g_pca
+Rscript GIT/cgp_new_pca_feat.R $drug $m_pca $g_pca
 echo "Done building training sets"
 
-cgp_new_pca_mlp.py $drug $m_pca $g_pca
+python GIT/cgp_new_pca_mlp.py $drug $m_pca $g_pca
 echo "Done sending mlp job"
