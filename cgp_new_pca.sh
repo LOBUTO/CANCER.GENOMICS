c77e4a1151b25ff30b7a1e0dd0e6c5a2eb2f1983
jzamalloa <zamalloa@princeton.edu>
2016-08-02 00:23:44 -0400

    minor changes

--- cgp_new_pca.sh
+++ cgp_new_pca.sh
@@ -5,6 +5,9 @@ drug=$1
 m_pca=$2
 g_pca=$3
 
+export PATH=/usr/local/cuda/bin:/usr/local/cuda/lib64:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
 Rscript GIT/cgp_new_pca_feat.R $drug $m_pca $g_pca
 echo "Done building training sets"
 
