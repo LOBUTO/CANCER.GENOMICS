a3ffb41d478f14ff60b0ac8e272e98d40f29cba3
jzamalloa <zamalloa@princeton.edu>
2016-08-08 00:16:03 -0700

    minor fix

--- cgp_new_sel_nci_feat.R
+++ cgp_new_sel_nci_feat.R
@@ -34,7 +34,9 @@ drug_table   <- rbind(drug_hmdb[METABOLITE %in% common_met,],
 
 drug_table   <- cor( acast(drug_table, METABOLITE~DRUG, value.var = "TC")  , method="spearman")
 drug_table   <- drug_table[target_drug, unique(MET.PROFILE$DRUG)]
+print(drug_table)
 drug_table   <- data.table(drug_table, keep.rownames=T)
+print(drug_table)
 
 setnames(drug_table, c("Compound", colnames(drug_table)[2:ncol(drug_table)]))
 
