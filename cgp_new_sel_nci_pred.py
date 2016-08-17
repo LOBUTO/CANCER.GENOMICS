ccc44f1f704b0c008707e11aeed85e8195821f02
jzamalloa <zamalloa@princeton.edu>
2016-08-17 03:55:44 -0400

    minor fixes

--- cgp_new_sel_nci_pred.py
+++ cgp_new_sel_nci_pred.py
@@ -386,7 +386,7 @@ usage       = sys.argv[3]
 
 if "-" not in drug_target:
     model_file  = (" ").join(model_file.split("-"))
-if drug_target=="Mitomycin_C":
+if drug_target=="Mitomycin C":
     model_file  = "CGP_FILES/CGP_NEW_RESULTS/new_cgp_sel_model_new_combat_Mitomycin C.pkl"
 
 in_folder   = "/home/zamalloa/Documents/FOLDER/CGP_FILES/TRAIN_TABLES/"
--- cgp_new_sel_nci_pred_wrapper.sh
+++ cgp_new_sel_nci_pred_wrapper.sh
@@ -6,12 +6,13 @@ model_end=".pkl"
 extra="new_combat"
 usage=$1
 
-for drug in Dasatinib Camptothecin Mitomycin_C Imatinib Cisplatin Sunitinib Midostaurin 17-AAG \
-Gefitinib Nilotinib Doxorubicin Vorinostat Gemcitabine Cytarabine \
-Axitinib Vinorelbine Methotrexate Shikonin Etoposide Paclitaxel Embelin Cyclopamine \
-PAC-1 Bleomycin Docetaxel Rapamycin ATRA Sorafenib Erlotinib Temsirolimus Parthenolide Lapatinib \
-Pazopanib Vinblastine Bortezomib Pyrimethamine Elesclomol Roscovitine
+# for drug in Dasatinib Camptothecin Mitomycin_C Imatinib Cisplatin Sunitinib Midostaurin 17-AAG \
+# Gefitinib Nilotinib Doxorubicin Vorinostat Gemcitabine Cytarabine \
+# Axitinib Vinorelbine Methotrexate Shikonin Etoposide Paclitaxel Embelin Cyclopamine \
+# PAC-1 Bleomycin Docetaxel Rapamycin ATRA Sorafenib Erlotinib Temsirolimus Parthenolide Lapatinib \
+# Pazopanib Vinblastine Bortezomib Pyrimethamine Elesclomol Roscovitine
 # for drug in PLX4720 TAE684 PHA-665752 Sorafenib PD-0325901 PD-0332991 Lapatinib 17-AAG Erlotinib Nilotinib Paclitaxel
+for drug in Mitomycin_C
 do
 
   model_file="$model_start$drug$model_end"
