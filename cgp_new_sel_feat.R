00f81e18dd90611bb06a019f67b4e08ddc064cf1
jzamalloa <zamalloa@princeton.edu>
2016-08-21 23:52:49 -0400

    added target feature modifier

--- cgp_new_sel_feat.R
+++ cgp_new_sel_feat.R
@@ -4,6 +4,75 @@
 library(data.table)
 library(reshape2)
 
+Function.cgp.select.best.feat <- function(cgp_new_feat, drug_met_cor, target_drug, weighted="length"){
+  # As specified, finds potential best data to train for drug
+
+  # Calculate weight contributions
+  drug_index   <- which(colnames(cgp_new_feat) %in% setdiff(colnames(drug_met_cor), "CMK") ) #To avoid confusion with CMK cell
+  cell_index   <- setdiff(1:ncol(cgp_new_feat), c(1:3, drug_index))
+
+  if (weighted=="length"){
+    cell_weight  <- length(cell_index) / (length(cell_index) + length(drug_index))
+    drug_weight  <- length(drug_index) / (length(cell_index) + length(drug_index))
+  } else if (weighted=="sd"){
+    cell_sd      <- apply(cgp_new_feat[, cell_index, with=F], 2, sd)
+    drug_sd      <- apply(cgp_new_feat[, drug_index, with=F], 2, sd)
+    cell_weight  <- sum(cell_sd) / (sum(cell_sd) + sum(drug_sd))
+    drug_weight  <- sum(drug_sd) / (sum(cell_sd) + sum(drug_sd))
+  } else {
+    cell_weight <- 0.5
+    drug_weight <- 0.5
+  }
+
+  # Calculate average contribution of target table
+  target_table <- cgp_new_feat[Compound==target_drug,]
+  target_cell  <- cor(t(target_table[,cell_index,with=F]), method = "pearson")
+  target_mean  <- mean(data.table(melt(target_cell))[Var1!=Var2, ]$value) * cell_weight + 1 * drug_weight
+
+  #Obtain correlation tables for drug and cell line based on features
+  cell_cor     <- cgp_new_feat[,c(2, cell_index),with=F]
+  setkey(cell_cor)
+  cell_cor     <- unique(cell_cor)
+  cell_cor     <- as.matrix(data.frame(cell_cor, row.names = 1))
+  cell_cor     <- cor(t(cell_cor), method = "pearson")
+  cell_cor     <- melt(cell_cor)
+  setnames(cell_cor, c("cell_name", "Cell", "cell_cor"))
+
+  drug_cor     <- cgp_new_feat[,c(1, drug_index),with=F]
+  setkey(drug_cor)
+  drug_cor     <- unique(drug_cor)
+  drug_cor     <- as.matrix(data.frame(drug_cor, row.names = 1))
+  drug_cor     <- cor(t(drug_cor), method = "pearson")
+  drug_cor     <- melt(drug_cor)
+  setnames(drug_cor, c("Compound", "Drug", "drug_cor"))
+
+  # Calculate correlation table between cell and drug features for target samples vs rests
+  main_table   <- cgp_new_feat[Compound!=target_drug,][,c("Compound", "cell_name"),with=F]
+  setkey(main_table)
+  main_table   <- unique(main_table)
+  main_table$c <- 1
+
+  to_table     <- cgp_new_feat[Compound==target_drug,][,c("Compound", "cell_name"),with=F] # To target table
+  setkey(to_table)
+  to_table     <- unique(to_table)
+  setnames(to_table, c("Drug", "Cell"))
+  to_table$c   <- 1
+
+  main_table   <- merge(main_table, to_table, by="c", allow.cartesian=T)
+  main_table   <- merge(main_table, cell_cor, by=c("cell_name", "Cell"))
+  main_table   <- merge(main_table, drug_cor, by=c("Compound", "Drug"))
+
+  # Calculate filter table for combined weights
+  main_table$weighted_score <- main_table$cell_cor * cell_weight + main_table$drug_cor * drug_weight
+  main_table   <- main_table[,list(final_score = mean(weighted_score)), by=c("Compound", "cell_name")]
+  main_table   <- main_table[final_score >= target_mean,]
+
+  # Return filtered features table
+  main_table   <- merge(cgp_new_feat, main_table[,c("Compound", "cell_name"),with=F], by=c("Compound", "cell_name"))
+  main_table   <- rbind(main_table, target_table)
+  return(main_table)
+}
+
 ######################################################################################################
 # LOAD DATA
 args        <- commandArgs(trailingOnly = TRUE)
@@ -35,7 +104,7 @@ temp_table  <- feat_table[Compound!=target_drug]
 if (modifier=="target_cells"){
   target_cells <- unique(test_table$cell_name)
   temp_table   <- temp_table[cell_name %in% target_cells,]
-  temp_table[,NORM_pIC50:=scale(NORM_pIC50), by="Compound"]
+  #temp_table[,NORM_pIC50:=scale(NORM_pIC50), by="Compound"]
 
 } else if (modifier=="target_drugs"){
   drug_met_cor <- cor( acast(MET.PROFILE, METABOLITE~DRUG, value.var = "TC")  , method="pearson")
@@ -53,6 +122,10 @@ if (modifier=="target_cells"){
   target_drugs <- unique(drug_met_cor$Var2)
   target_cells <- unique(test_table$cell_name)
   temp_table   <- temp_table[Compound %in% target_drugs,][cell_name %in% target_cells,]
+
+} else if (modifier=="target_feat"){
+  temp_table   <- Function.cgp.select.best.feat(feat_table, drug_met_cor, target_drug, weighted="length")
+  temp_table   <- temp_table[Compound!=target_drug,]
 
 } else {
   print("no modifier")
