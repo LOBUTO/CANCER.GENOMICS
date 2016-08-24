9aee7c706e618c496294721b0c727d102c7f5104
jzamalloa <zamalloa@princeton.edu>
2016-08-24 12:25:49 -0400

    minor fix

--- cgp_new_sel_mlp_tigress.sh
+++ cgp_new_sel_mlp_tigress.sh
@@ -13,7 +13,8 @@ modifier=$3
 
 # for drug in Erlotinib Lapatinib Vorinostat Elesclomol ATRA Gefitinib Parthenolide Cyclopamine 17-AAG \
 # Sunitinib Gemcitabine Doxorubicin Vinorelbine Vinblastine Mitomycin_C Embelin Paclitaxel
-for drug in Erlotinib Lapatinib Gefitinib Sunitinib Gemcitabine Doxorubicin
+#for drug in Erlotinib Lapatinib Gefitinib Sunitinib Gemcitabine Doxorubicin
+for drug in Erlotinib Lapatinib Gefitinib Gemcitabine
 do
   Rscript GIT/cgp_new_sel_feat.R $drug $usage $modifier
   echo "Done building training sets"
