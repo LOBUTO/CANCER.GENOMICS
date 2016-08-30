d2ed90ed5cdff7bf66c9c88efe5f9d4ba56c3c0c
jzamalloa <zamalloa@princeton.edu>
2016-08-30 16:18:11 -0400

    building pipeline for tcga input and prediction

--- cgp_new_sel_predict_tcga.sh
+++ cgp_new_sel_predict_tcga.sh
@@ -3,14 +3,21 @@
 
 cancer=$1
 cancer_exp=$2
-target_drug=$3
+drug=$3
 
 usage="tcga_$cancer"
 modifier=$4
+extra=$5
 
 # Set up entry table
-Rscript GIT/cgp_new_sel_predict_tcga_prep.R $cancer $cancer_exp $target_drug
+Rscript GIT/cgp_new_sel_predict_tcga_prep.R $cancer $cancer_exp $drug
 echo "Done building cgp feat table"
 
 # Split for training
-Rscript GIT/cgp_new_sel_feat.R $target_drug $usage $modifier
+Rscript GIT/cgp_new_sel_feat.R $drug $usage $modifier
+echo "Done drug table for training"
+
+# Train
+export drug usage extra modifier
+sbatch GIT/cgp_new_sel_mlp_tigress.cmd
+echo "Done sending mlp job"
