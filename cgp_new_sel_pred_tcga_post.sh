5f0c4d2095ba993e12b365baab9f61098072be1a
jzamalloa <zamalloa@princeton.edu>
2016-08-31 01:43:49 -0400

    minor fixes

--- cgp_new_sel_pred_tcga_post.sh
+++ cgp_new_sel_pred_tcga_post.sh
@@ -5,6 +5,9 @@ cancer=$1
 modifier=$2
 extra=$3
 
+export PATH=/usr/local/cuda/bin:/usr/local/cuda/lib64:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
 for drug in Docetaxel Paclitaxel Tamoxifen
 do
 
