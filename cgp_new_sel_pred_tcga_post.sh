1eb4e0e85099d4d6bdc72d8202ab7af257c41a76
jzamalloa <zamalloa@princeton.edu>
2016-08-31 12:25:38 -0400

    minor fixes

--- cgp_new_sel_pred_tcga_post.sh
+++ cgp_new_sel_pred_tcga_post.sh
@@ -8,7 +8,8 @@ extra=$3
 export PATH=/usr/local/cuda/bin:/usr/local/cuda/lib64:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-for drug in Docetaxel Paclitaxel Tamoxifen
+#for drug in Docetaxel Paclitaxel Tamoxifen
+for drug in Paclitaxel Carboplatin
 do
 
   echo $drug
