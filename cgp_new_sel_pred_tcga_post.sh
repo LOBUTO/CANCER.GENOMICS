5f8fe6c4ab55ab080b0d5f1cc4c9d4c879bcc731
jzamalloa <zamalloa@princeton.edu>
2016-09-01 16:06:40 -0400

    modify master tcga grapher to include more than one cancer

--- cgp_new_sel_pred_tcga_post.sh
+++ cgp_new_sel_pred_tcga_post.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 # cgp_new_sel_pred_tcga_post.sh
 
-cancer=$1
-modifier=$2
-extra=$3
+modifier=$1
+extra=$2
 
 export PATH=/usr/local/cuda/bin:/usr/local/cuda/lib64:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-#for drug in Docetaxel Paclitaxel Tamoxifen
+for drug in Anastrozole Docetaxel Paclitaxel Tamoxifen
+do
+
+    cancer="brca"
+    echo $cancer
+
+    python GIT/cgp_new_sel_pred_tcga_post.py $drug $cancer $modifier $extra
+
+    Rscript GIT/cgp_new_sel_pred_tcga_plot.R $drug $cancer $extra
+
+done
+
 for drug in Paclitaxel Carboplatin
 do
 
+  cancer="ucec"
   echo $drug
   python GIT/cgp_new_sel_pred_tcga_post.py $drug $cancer $modifier $extra
 
   Rscript GIT/cgp_new_sel_pred_tcga_plot.R $drug $cancer $extra
 
 done
+
+for drug in Alimta Paclitaxel Carboplatin Cisplatin
+do
+
+  cancer="luad"
+  echo $drug
+  python GIT/cgp_new_sel_pred_tcga_post.py $drug $cancer $modifier $extra
+
+  Rscript GIT/cgp_new_sel_pred_tcga_plot.R $drug $cancer $extra
+
+done
+
 echo "DONE"
